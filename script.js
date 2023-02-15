148b28e4b3c552e082fa6529c2680b2c34f843ce
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-02-15 18:41:46 -0300

    decrypt working

--- script.js
+++ script.js
@@ -2,18 +2,22 @@
 
 function encrypt() {
   let text = document.getElementById('textEncrypt').value;
-
-  let asciiValues = [];
-  let charValues = [];
   let encrypted = '';
 
   for (let i = 0; i < text.length; i++) {
-    asciiValues.push(('00' + text[i].charCodeAt(0)).slice(-3));
-    encrypted += asciiValues[i];
-    charValues.push(String.fromCharCode(asciiValues[i]));
+    encrypted += ('00' + text[i].charCodeAt(0)).slice(-3);
+  }
+  document.getElementById('encrypted').value = encrypted;
+}
+
+function decrypt() {
+  let text = document.getElementById('textDecrypt').value;
+  let decrypted = '';
+  let splited = text.match(/.{1,3}/g) ?? [];
+
+  for (let i = 0; i < splited.length; i++) {
+    decrypted += String.fromCharCode(splited[i]);
   }
 
-  console.log(asciiValues);
-  console.log(charValues);
-  document.getElementById('encrypted').value = encrypted;
+  document.getElementById('decrypted').value = decrypted;
 }
