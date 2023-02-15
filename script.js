038e956465165f031276fdc8824d53cdadbda35e
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-02-15 18:02:15 -0300

    encrypt working

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function encrypt() {
+  let text = document.getElementById('textEncrypt').value;
+
+  let asciiValues = [];
+  let charValues = [];
+  let encrypted = '';
+
+  for (let i = 0; i < text.length; i++) {
+    asciiValues.push(('00' + text[i].charCodeAt(0)).slice(-3));
+    encrypted += asciiValues[i];
+    charValues.push(String.fromCharCode(asciiValues[i]));
+  }
+
+  console.log(asciiValues);
+  console.log(charValues);
+  document.getElementById('encrypted').value = encrypted;
+}
